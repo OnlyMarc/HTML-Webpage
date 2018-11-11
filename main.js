0cbbde18842f650546599d571e9f5e706f7e99cb
ThinkingEmote <38572524+OnlyMarc@users.noreply.github.com>
2018-11-11 17:08:39 +0100

    a

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
       // * Shrink
-      $('.navbar').css('cssText', 'background-color: #2a2a2a; padding: 10px 50px;')
+      $('.navbar ul').css('cssText', 'background-color: #2a2a2a;')
   } else {
       // * Normal
-      $('.navbar').css('cssText', 'background-color: none; padding: 20px 50px;')
+      $('.navbar ul').css('cssText', 'background-color: none;')
   }
 }
